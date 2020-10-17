04d37602a8860117648dab9b457b418bdc09849a
laurent vuillaume <figura.graphik@gmail.com>
2020-10-17 18:47:24 +0200

    add route tags, route /api/profile/me

---
+++ routes/tags.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = express.Router();
+const Tag = require("../models/Tag");
+
+
+/************* GET ALL CATEGORIES *************/
+router.get("/", async (req, res, next) => {
+    try {
+        const tags = await Tag.find({});
+        res.status(200).json({
+            tags,
+            message:"tags successfully loaded"
+        }); 
+    }catch(errDb) {
+        console.log(errDb);
+        res.status(500).json({
+            error: errDb,
+            message: 'Error, fail loading tags'
+        });
+    };
+})
+
+module.exports = router;
