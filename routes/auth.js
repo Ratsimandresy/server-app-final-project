29e5feeb0320556530c9fa783ff989e7b63b19ee
laurent vuillaume <figura.graphik@gmail.com>
2020-10-17 08:06:51 +0200

    up

--- routes/auth.js
+++ routes/auth.js
@@ -7,65 +7,71 @@ const User = require("../models/User");
 const salt = 10;
 
 router.post("/signin", (req, res, next) => {
-  const { email, password } = req.body;
-  User.findOne({ email })
-    .then((userDocument) => {
-      if (!userDocument) {
-        return res.status(400).json({ message: "Invalid credentials" });
-      }
+    const {email, password} = req.body;
+    console.log(email, password);
+    User.findOne({email}).then((userDocument) => {
+        if (!userDocument) {
+            return res.status(400).json({message: "Invalid credentials"});
+        }
 
-      const isValidPassword = bcrypt.compareSync(password, userDocument.password);
-      if (!isValidPassword) {
-        return res.status(400).json({ message: "Invalid credentials" });
-      }
-      
-      req.session.currentUser = userDocument._id;
-      res.redirect("/api/auth/isLoggedIn");
-    })
-    .catch(next);
+        const isValidPassword = bcrypt.compareSync(password, userDocument.password);
+        if (! isValidPassword) {
+            return res.status(400).json({message: "Invalid credentials"});
+        }
+
+        req.session.currentUser = userDocument._id;
+        res.redirect("/api/auth/isLoggedIn");
+    }).catch(next);
 });
 
 router.post("/signup", (req, res, next) => {
-  const { email, password, firstName, lastName } = req.body;
+    console.log(req.body);
+    const {email, password, confirmPassword, firstName, gender, lastName} = req.body;
 
-  User.findOne({ email })
-    .then((userDocument) => {
-      if (userDocument) {
-        return res.status(400).json({ message: "Email already taken" });
-      }
+    User.findOne({email}).then((userDocument) => {
+        if (userDocument) {
+            return res.status(400).json({message: "Email already taken"});
+        }
+        
+        if(confirmPassword !== password ) {
+          return res.status(400).json({message: "Error your password and your confirmed password are not the same"});
+        }
 
-      const hashedPassword = bcrypt.hashSync(password, salt);
-      const newUser = { email, lastName, firstName, password: hashedPassword };
+        const hashedPassword = bcrypt.hashSync(password, salt);
+        const newUser = {
+            email,
+            lastName,
+            firstName,
+            gender,
+            password: hashedPassword
+        };
 
-      User.create(newUser)
-        .then((newUserDocument) => {
-          /* Login on signup */
-          req.session.currentUser = newUserDocument._id;
-          res.redirect("/api/auth/isLoggedIn");
-        })
-        .catch(next);
-    })
-    .catch(next);
+        User.create(newUser).then((newUserDocument) => { /* Login on signup */
+            req.session.currentUser = newUserDocument._id;
+            res.redirect("/api/auth/isLoggedIn");
+        }).catch(next);
+    }).catch(next);
 });
 
 router.get("/isLoggedIn", (req, res, next) => {
-  if (!req.session.currentUser)
-    return res.status(401).json({ message: "Unauthorized" });
+    if (!req.session.currentUser) 
+        return res.status(401).json({message: "Unauthorized"});
+    
 
-  const id = req.session.currentUser;
-  User.findById(id)
-    .select("-password")
-    .then((userDocument) => {
-      res.status(200).json(userDocument);
-    })
-    .catch(next);
+    const id = req.session.currentUser;
+    User.findById(id).select("-password").then((userDocument) => {
+        res.status(200).json(userDocument);
+    }).catch(next);
 });
 
 router.get("/logout", (req, res, next) => {
-  req.session.destroy(function (error) {
-    if (error) next(error);
-    else res.status(200).json({ message: "Succesfully disconnected." });
-  });
+    req.session.destroy(function (error) {
+        if (error) 
+            next(error);
+         else 
+            res.status(200).json({message: "Succesfully disconnected."});
+        
+    });
 });
 
 module.exports = router;
