5f35f43dba5e04ad54ec0a4748afe339cf9ab98b
Lcdevi <caillauxlaura@gmail.com>
2020-10-18 16:09:58 +0200

    fixed bug

--- routes/users.js
+++ routes/users.js
@@ -13,14 +13,7 @@ router.get("/", (req, res, next) => {
     });
 });
 
-router.get("/:id", (req, res, next) => {
-    const userID = req.params.id;
-    User.findById(userID).then((userRes) => {
-        res.status(200).json(userRes);
-    }).catch((err) => {
-        res.status(200).json(err);
-    });
-});
+
 
 router.get("/me", async (req, res, next) => {
   try{
@@ -83,6 +76,15 @@ router.patch("/update", uploadCloud.single("profilImage"), async (req, res, next
     });
   }
  
+});
+
+router.get("/:id", (req, res, next) => {
+  const userID = req.params.id;
+  User.findById(userID).then((userRes) => {
+      res.status(200).json(userRes);
+  }).catch((err) => {
+      res.status(200).json(err);
+  });
 });
 
 router.delete("/:id", (req, res, next) => {
